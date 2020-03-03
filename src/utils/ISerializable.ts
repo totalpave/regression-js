3cbbb5abbdfd764ca44802be958544b9dd1ca365
Norman Breau <norman@normanbreau.com>
2020-03-03 11:46:40 -0400

    Removed ISerializable in favor for @totalpave/object serializable interface

--- src/utils/ISerializable.ts
+++
@@ -1,8 +0,0 @@
-
-type BasicTypes = number | string | boolean | Array<number | string | boolean> | {[key: string]: number | string | boolean};
-
-export interface ISerializable {
-    [key: string]: BasicTypes | Array<BasicTypes>;
-}
-
-export default ISerializable;
