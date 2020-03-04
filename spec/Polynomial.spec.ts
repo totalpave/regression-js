a163cb210ef4d5645013caea092e01e4fd255a6a
Norman Breau <norman@normanbreau.com>
2020-03-04 09:14:27 -0400

    test: Polynomials

--- spec/Polynomial.spec.ts
+++ spec/Polynomial.spec.ts
@@ -16,4 +16,25 @@ describe('Polynomial', () => {
         });
         expect(p.findX(100)).toBe(0);
     });
+
+    it('derivative', () => {
+        let p: Polynomial = new Polynomial([
+            -7.96390941104619e-6,
+            0.000650882546324283,
+            -0.0194003954529762,
+            0.255068331956863,
+            -1.39548635482788,
+            -0.56826284599304,
+            100
+        ], {
+            precision: 10,
+            xRange: {
+                preferLowerX: true,
+                low: 0,
+                high: 35
+            }
+        });
+
+        expect(p.derivative(12)).toBe(-2.3724999639838167);
+    });
 });
